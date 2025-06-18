565acecf8dc53e4fd21f7080a097b798f543b182
MintHerbs <hexanians@gmail.com>
2025-06-18 19:00:28 +0400

    Initial commit from local project

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  SafeAreaView,
+  StatusBar,
+  Platform,
+  View,
+} from "react-native";
+
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+  
+  return <WelcomeScreen />;
+
+}
