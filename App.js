223af1ef33d0878716fefa810a4d6aeba571cb53
MintHerbs <hexanians@gmail.com>
2025-07-01 17:43:51 +0400

    stage 7

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import AccountScreen from './app/screens/AccountScreen';
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import React from "react";
 
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
+import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return <WelcomeScreen />; // Change to <AccountScreen /> to view the account screen
+  return <AccountScreen />;
 }
-
